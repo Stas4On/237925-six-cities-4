5fd7fc1a646da5ddd6e98e5d812e681ef4d474d7
stas_kuznetsov <stanislav.kuznetsov@rokolabs.com>
2020-07-05 15:31:26 +0400

    Fixes tests

---
+++ src/mocks/test-mocks.ts
@@ -0,0 +1,117 @@
+import {OfferModel} from "../models";
+
+export const testOffers: OfferModel[] = [
+  {
+    city: {
+      name: `Paris`,
+      location: {
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 13
+      }
+    },
+    name: `Beautiful &amp; luxurious apartment at great location`,
+    price: 120,
+    rating: 4,
+    type: `apartment`,
+    imgUrl: `img/room.jpg`,
+    description: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century. An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`,
+    features: [`Wi-Fi`, `Washing machine`, `Towels`, `Heating`, `Coffee machine`, `Baby seat`, `Kitchen`, `Dishwasher`, `Cabel TV`, `Fridge`],
+    photos: [`img/room.jpg`, `img/apartment-03.jpg`, `img/room.jpg`, `img/apartment-02.jpg`, `img/room.jpg`, `img/apartment-01.jpg`],
+    coordinates: [52.3909553943508, 4.85309666406198],
+    bedrooms: 2,
+    guests: 4,
+    owner: `Angelina`,
+    avatar: `img/avatar-angelina.jpg`,
+    favorite: false,
+    premium: true,
+    id: 12
+  },
+  {
+    city: {
+      name: `Hamburg`,
+      location: {
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 13
+      }
+    },
+    name: `Wood and stone place`,
+    price: 80,
+    rating: 3.5,
+    type: `private room`,
+    imgUrl: `img/room.jpg`,
+    description: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century. An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`,
+    features: [`Wi-Fi`, `Washing machine`, `Baby seat`, `Kitchen`, `Dishwasher`, `Cabel TV`, `Fridge`],
+    photos: [`img/room.jpg`, `img/apartment-03.jpg`, `img/room.jpg`, `img/apartment-02.jpg`, `img/room.jpg`, `img/apartment-01.jpg`],
+    coordinates: [52.369553943508, 4.85309666406198],
+    bedrooms: 2,
+    guests: 4,
+    owner: `Angelina`,
+    avatar: `img/avatar-angelina.jpg`,
+    favorite: false,
+    premium: false,
+    id: 31
+  },
+  {
+    city: {
+      name: `Amsterdam`,
+      location: {
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 13
+      }
+    },
+    name: `Canal View Prinsengracht`,
+    price: 132,
+    rating: 2.33,
+    type: `apartment`,
+    imgUrl: `img/room.jpg`,
+    description: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century. An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`,
+    features: [`Wi-Fi`, `Washing machine`, `Towels`, `Heating`, `Coffee machine`],
+    photos: [`img/room.jpg`, `img/apartment-03.jpg`, `img/room.jpg`, `img/apartment-02.jpg`, `img/room.jpg`, `img/apartment-01.jpg`],
+    coordinates: [52.3909553943508, 4.929309666406198],
+    bedrooms: 2,
+    guests: 4,
+    owner: `Angelina`,
+    avatar: `img/avatar-angelina.jpg`,
+    favorite: false,
+    premium: false,
+    id: 23
+  },
+  {
+    city: {
+      name: `Amsterdam`,
+      location: {
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 13
+      }
+    },
+    name: `Nice, cozy, warm big bed apartment`,
+    price: 180,
+    rating: 4.5,
+    type: `apartment`,
+    imgUrl: `img/room.jpg`,
+    description: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century. An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`,
+    features: [`Wi-Fi`, `Kitchen`, `Dishwasher`, `Cabel TV`, `Fridge`],
+    photos: [`img/room.jpg`, `img/apartment-03.jpg`, `img/room.jpg`, `img/apartment-02.jpg`, `img/room.jpg`, `img/apartment-01.jpg`],
+    coordinates: [52.3809553943508, 4.939309666406198],
+    bedrooms: 2,
+    guests: 4,
+    owner: `Angelina`,
+    avatar: `img/avatar-angelina.jpg`,
+    favorite: false,
+    premium: true,
+    id: 1
+  }
+];
+
+export const cities = [
+  `Paris`,
+  `Cologne`,
+  `Brussels`,
+  `Amsterdam`,
+  `Hamburg`,
+  `Dusseldorf`
+];
