0278a9aa7c5a8c17644af4556a59f1008cb058cb
stas_kuznetsov <stanislav.kuznetsov@rokolabs.com>
2020-07-07 12:23:21 +0400

    Updates snapshot tests

--- src/components/main/main.test.js
+++ src/components/main/main.test.js
@@ -2,16 +2,28 @@ import React from "react";
 import renderer from "react-test-renderer";
 import {cities, testOffers} from "../../mocks/test-mocks";
 import Main from "./main";
+import {Provider} from "react-redux";
+import configureStore from "redux-mock-store";
+
+
+const mockStore = configureStore([]);
+const store = mockStore({
+  offers: testOffers,
+  currentCity: cities[0],
+  cities,
+});
 
 it(`<Main /> should render cards offers`, () => {
   const tree = renderer
-    .create(<Main
-      onChangeCity = {() => {}}
-      offers={testOffers}
-      onTitleCardClick={() => {}}
-      city = {cities[0]}
-      cities={cities}
-    />).toJSON();
+    .create(
+        <Provider store={store}>
+          <Main
+            onChangeCity={() => {}}
+            offers={testOffers}
+            onTitleCardClick={() => {}}
+            cities={cities}
+            currentCity={cities[0]}/>
+        </Provider>).toJSON();
 
   expect(tree).toMatchSnapshot();
 });
